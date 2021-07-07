e4e8b7a880afde3ef464aef222630bb06deb9624
lichifinance <86365196+lichifinance@users.noreply.github.com>
2021-07-08 00:05:28 +0200

    Add files via upload

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -30,62 +30,16 @@ const config: MenuEntry[] = [
     icon: 'PoolIcon',
     href: '/nests',
   },
-  // {
-  //   label: 'Pools',
-  //   icon: 'PoolIcon',
-  //   href: '/pools',
-  // },
-  // {
-  //   label: 'Lottery',
-  //   icon: 'TicketIcon',
-  //   href: '/lottery',
-  // },
-  // {
-  //   label: 'NFT',
-  //   icon: 'NftIcon',
-  //   href: '/nft',
-  // },
   {
-    label: 'Info',
-    icon: 'InfoIcon',
-    items: [
-      {
-        label: 'PancakeSwap',
-        href: 'https://pancakeswap.info/token/0xe32c2f4065e911ed678c8bee47289a9b3d39f638',
-      },
-	    {
-         label: 'Docs',
-         href: 'https://app.gitbook.com/@smokingcucumberfinance/s/smokingcucumber/',
-     },
-      // {
-      //   label: 'CoinGecko',
-      //   href: 'https://www.coingecko.com/en/coins/goose-finance',
-      // },
-      // {
-      //   label: 'CoinMarketCap',
-      //   href: 'https://coinmarketcap.com/currencies/goose-finance/',
-      // },
-      // {
-      //   label: 'AstroTools',
-      //   href: 'https://app.astrotools.io/pancake-pair-explorer/0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
-      // },
-    ],
+    label: 'Twitter',
+    icon: 'TwitterIcon',
+    href: 'https://twitter.com/SmokingCucumber',
   },
- /*  {
-   label: 'Doc',
-    icon: 'MoreIcon',
-    items: [ 
-      /* {
-        label: 'Github',
-        href: 'https://github.com/chessfinance/',
-      }, 
-     
-     {
-        label: 'Blog',
-        href: 'https://chessdefi.medium.com/',
-      }, 
-    ], 
-  }, */
+  {
+    label: 'Telegram',
+    icon: 'TelegramIcon',
+    href: 'https://t.me/SmokingCucumberFinance',
+  },
 ]
 
 export default config
