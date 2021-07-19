00f6874648a2de0a62b3814f72a4560d135d889f
lichifinance <86365196+lichifinance@users.noreply.github.com>
2021-07-19 11:39:12 -0400

    Update config.ts

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -73,6 +73,36 @@ const config: MenuEntry[] = [
     	icon: 'AuditIcon',
     	href: 'https://rugdoc.io/project/smoking-cucumber/',
     },
+    {
+    	label: "Listings",
+        icon: "HamburgerIcon",
+		items: [
+            {
+                label: "Vfat",
+                href: "https://vfat.tools/bsc/smokingcucumber/",
+            },
+            {
+                label: "CoinSniper",
+                href: "https://coinsniper.net/coin/8501",
+            },
+			{
+                label: "CoinScope",
+                href: "https://www.coinscope.co/coin/ccb",
+            },
+			{
+                label: "SmartObserver",
+                href: "https://smartobserver.online/token/0xd15d8744528231b1ef1775a0f4ff5275e7f74dea",
+            },
+			{
+                label: "Reddit CMS",
+                href: "https://www.reddit.com/r/CryptoMoonShots/comments/oiy1il/smoking_cucumber_new_yield_farm_on_the_bsc_100/",
+            },
+			{
+                label: "CoinGecko soon...",
+                href: "",
+            },
+        ],
+    },
 ]
 
 export default config
